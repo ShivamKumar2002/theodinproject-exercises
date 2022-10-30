36bc632f9239efd1b5d560df7c28260ad9940414
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2022-10-30 17:43:13 +0530

    Add UI in Rock Paper Scissors
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- Rock Paper Scissors/rock-paper-scissors.js
+++ Rock Paper Scissors/rock-paper-scissors.js
@@ -1,4 +1,12 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["Rock", "Paper", "Scissor"];
+
+const choiceButtons = document.getElementsByClassName("choice-button");
+const playerScoreDiv = document.getElementById("playerScore");
+const computerScoreDiv = document.getElementById("computerScore");
+const lastResultDiv = document.getElementById("lastResult");
+
+let playerScore = 0;
+let computerScore = 0;
 
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -12,56 +20,63 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function incrementScore(winner) {
+    if (winner === "player") {
+        playerScore += 1;
+        playerScoreDiv.innerText = playerScore;
+    } else if (winner === "computer") {
+        computerScore += 1;
+        computerScoreDiv.innerText = computerScore;
+    } else {
+        console.log("Invalid winner received in incrementScore")
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDiv.innerText = "0";
+    computerScoreDiv.innerText = "0";
+    lastResultDiv.innerText = "";
+}
+
+function checkWinner() {
+    if (playerScore >= 5) {
+        alert("Congrats! You have won.");
+        resetGame();
+    } else if (computerScore >= 5) {
+        alert("Oh no! You lost.");
+        resetGame();
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
     let playerChoiceIndex = getChoiceIndex(playerSelection);
     let computerChoiceIndex = getChoiceIndex(computerSelection);
 
     if (playerChoiceIndex === 0 && computerChoiceIndex === 2) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        incrementScore("player");
+        lastResultDiv.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+
     } else if (playerChoiceIndex === 2 && computerChoiceIndex === 0) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        incrementScore("computer");
+        lastResultDiv.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+
     } else if (playerChoiceIndex > computerChoiceIndex) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        incrementScore("player");
+        lastResultDiv.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+
     } else if (computerChoiceIndex > playerChoiceIndex) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        incrementScore("computer");
+        lastResultDiv.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+
     } else {
-        return `Draw! Computer Chose: ${playerSelection}, Player Chose: ${computerSelection}`;
+        lastResultDiv.innerText = `Draw! Computer Chose: ${playerSelection}, Player Chose: ${computerSelection}`;
     }
+
+    checkWinner();
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = capitalize(prompt("What is your choice? Rock, Paper, Scissors?: "));
-        if (!choices.includes(playerSelection)) {
-            alert("Invalid Choice!");
-            i -= 1;
-            continue;
-        }
-
-        let roundResult = playRound(playerSelection, getComputerChoice());
-        console.log(roundResult);
-
-        if (roundResult.contains("Win")) {
-            playerScore += 1;
-        } else if (roundResult.contains("Lose")) {
-            computerScore += 1;
-        } else {
-            // Player and computer both get one point in case of draw
-            playerScore += 1;
-            computerScore += 1;
-        }
-    }
-
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-
-    if (playerScore > computerScore) {
-        console.log("Congrats! You Win.")
-    } else if (playerScore < computerScore) {
-        console.log("Oh No! You Lost.")
-    } else {
-        console.log("Draw! Excellent Game.")
-    }
+for (let choiceButton of choiceButtons) {
+    choiceButton.addEventListener("click", () => playRound(choiceButton.textContent, getComputerChoice()));
 }
